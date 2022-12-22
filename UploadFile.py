ceabb1ffea85c1c3703e17905b09ce9b720e96fb
LaloRdz <rodela22@hotmail.com>
2022-12-22 13:56:28 -0600

    Upload File to S3

---
+++ UploadFile.py
@@ -0,0 +1,36 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
+
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    """
+        Taken from
+        https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
+    """
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+s3 = boto3.client('s3')
+with open("tcs_logo.jpg", "rb") as f:
+    s3.upload_fileobj(f, "testbucketlalordz", "logo")

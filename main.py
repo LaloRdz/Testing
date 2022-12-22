c34972984686aed911c1ad7eb869ffc44ea03a55
LaloRdz <rodela22@hotmail.com>
2022-12-22 11:02:26 -0600

    Retrive bucket list in amazon S3 using Boto3

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-try:
-    open("database.sqlite")
-except OSError:
-    raise RuntimeError("unable to handle error")
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+
+# Retrieve the list of existing buckets
+s3 = boto3.client('s3')
+response = s3.list_buckets()
+
+# Output the bucket names
+print('Existing buckets:')
+for bucket in response['Buckets']:
+    print(f'  {bucket["Name"]}')
